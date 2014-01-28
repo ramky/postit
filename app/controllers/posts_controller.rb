a88dcb7cd8946c558e5ad8aaa6c6c93babf59ed6
Ram Iyer <ramky.iyer@yahoo.com>
2014-01-28 14:30:45 -0800

    adding post resource

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,21 @@
 class PostsController < ApplicationController
   def index
+  	@posts = Post.all
+  end
 
+  def show
+  	@post = Post.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
   end
 end
