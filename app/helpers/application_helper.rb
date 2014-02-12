5f1d149a91b01e8219104e224df95d1776b823fe
Ram Iyer <ramky.iyer@yahoo.com>
2014-02-12 14:48:09 -0800

    adding helper for formatting datetime

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,8 @@ module ApplicationHelper
 	def fix_url(str)
 		str.starts_with?('http://') ? str : "http://#{str}"
 	end
+
+	def display_datetime(dt)
+		dt.strftime("%m/%d/%Y %l:%M%P %Z")
+	end
 end
