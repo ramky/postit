feba9ae9c0847a8231b3a0d262045905287747c2
Ram Iyer <ramky.iyer@yahoo.com>
2014-02-12 11:55:04 -0800

    correcting error message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
 	def model_name(instance)
 		instance.class.to_s
 	end
+
+	def error_text(instance)
+		if instance.errors.count == 1
+			"1 error"
+		else
+			"#{instance.errors.count} errors"
+		end
+	end
 end
